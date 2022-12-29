427f389cd22a486e4efa459a341e88cc4569d8c2
Imacder <sunlili209@gmail.com>
2022-12-29 17:32:22 +0800

    Initial Commit

---
+++ SecureNotes/SecureNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureNotesApp.swift
+//  SecureNotes
+//
+//  Created by Albert Kong on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SecureNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
